ecfd50b4e87c71c9ed8470e25cf57db4e95f0211
Casey Foster <c@sey.me>
2014-01-03 11:27:42 -0600

    Define as anonymous module, 0.2.2

--- dpr.js
+++ dpr.js
@@ -1,6 +1,6 @@
 (function (root, factory) {
   if (typeof root.define === 'function' && root.define.amd) {
-    root.define('dpr', ['jquery'], factory);
+    root.define(['jquery'], factory);
   } else if (typeof exports !== 'undefined') {
     module.exports = factory(require('jquery'));
   } else {
